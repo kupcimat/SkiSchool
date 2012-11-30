94c9f0026d467855a4194dd78c66c3e0296a9b3c
gias <sabol.miso@gmail.com>
2012-12-01 00:55:14 +0100

    FEATURE: Added Instrustor administration

---
+++ jakaski/app/controllers/AdminInstructor.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import models.Instructor;
+import controllers.deadbolt.Deadbolt;
+import controllers.deadbolt.Restrict;
+import play.mvc.Before;
+import play.mvc.Controller;
+import play.mvc.With;
+
+@With(Deadbolt.class)
+@Restrict("instructor")
+public class AdminInstructor extends Controller {
+
+	   @Before
+	    static void setConnectedUser() {
+	        if(Security.isConnected()) {
+	            Instructor instructor = Instructor.find("byEmail", Security.connected()).first();
+	            renderArgs.put("user", instructor.email);
+	        }
+	    }
+	 
+	    public static void instructor() {
+	        render();
+	    }
+
+}
