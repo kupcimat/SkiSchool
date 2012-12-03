b2099e7771f570e4dfc725508cc412fa1b92ac6e
gias <sabol.miso@gmail.com>
2012-12-03 01:55:02 +0100

    FEATURE: Customizing and styling of Students CRUD modul

--- jakaski/app/controllers/Instructors.java
+++ jakaski/app/controllers/Instructors.java
@@ -1,11 +1,20 @@
 package controllers;
 
+import models.User;
+import play.mvc.Before;
 import play.mvc.With;
 import controllers.deadbolt.Deadbolt;
 import controllers.deadbolt.Restrict;
 
 @With(Deadbolt.class)
 @Restrict("admin")
-public class Instructors extends CRUD{
-
+public class Instructors extends CRUD {
+	
+	@Before
+	static void setConnectedUser() {
+		if (Security.isConnected()) {
+			User user = User.find("byEmail", Security.connected()).first();
+			renderArgs.put("instructor", user);
+		}
+	}
 }
--- jakaski/app/controllers/Students.java
+++ jakaski/app/controllers/Students.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.User;
+import play.mvc.Before;
 import play.mvc.With;
 import controllers.deadbolt.Deadbolt;
 import controllers.deadbolt.Restrict;
@@ -8,4 +10,11 @@ import controllers.deadbolt.Restrict;
 @Restrict("admin")
 public class Students extends CRUD{
 
+	@Before
+	static void setConnectedUser() {
+		if (Security.isConnected()) {
+			User user = User.find("byEmail", Security.connected()).first();
+			renderArgs.put("instructor", user);
+		}
+	}
 }
