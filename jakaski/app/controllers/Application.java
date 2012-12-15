472b8cadcd2689d6f8c06adbbf903c009f0740b5
Matej Kupciha <kupciham@hotmail.com>
2012-12-15 19:26:39 +0100

    REFACTOR: Removing unused code

--- jakaski/app/controllers/Application.java
+++ jakaski/app/controllers/Application.java
@@ -1,16 +1,10 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
+import play.mvc.Controller;
+import play.mvc.With;
 import controllers.deadbolt.Deadbolt;
 import controllers.deadbolt.Restrict;
 import controllers.deadbolt.Restrictions;
-import controllers.scheduler.SchedulerEvent;
-
-import models.*;
 
 @With(Deadbolt.class)
 public class Application extends Controller {
@@ -20,22 +14,4 @@ public class Application extends Controller {
         render();
     }
 
-    /**
-     * @param body MUST BE NAMED "BODY" TO FUNCTION PROPERLY (BECAUSE WE USE BODY OF HTTP REQUEST)!
-     */
-    public static void createAvailability(SchedulerEvent body) {
-
-        String user = Security.connected();
-        Instructor instructor = Instructor.find("byEmail", user).first();
-
-        // TODO debug
-        // System.err.println("body: " + body);
-        // System.out.println("user: " + user);
-        // System.out.println("instructor: " + instructor);
-
-        Availability availability = body.createAvailability(new Location("Jahodna"), instructor);
-        instructor.addAvailability(availability);
-
-        renderJSON(new SchedulerEvent.Response(availability.getId()));
-    }
 }
