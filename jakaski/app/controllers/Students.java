ec748f70e1332ba6cfc4eb515cc0278e22e69602
Matej Kupciha <kupciham@hotmail.com>
2013-03-08 20:20:29 +0100

    FEATURE: Student overview statistic PoC

--- jakaski/app/controllers/Students.java
+++ jakaski/app/controllers/Students.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Student;
 import models.User;
 import play.mvc.Before;
 import play.mvc.With;
@@ -8,14 +12,29 @@ import controllers.deadbolt.Restrict;
 import controllers.deadbolt.Restrictions;
 
 @With(Deadbolt.class)
-@Restrictions({@Restrict("editor"), @Restrict("admin") })
-public class Students extends CRUD{
+@Restrictions({ @Restrict("editor"), @Restrict("admin") })
+public class Students extends CRUD {
 
-	@Before
-	static void setConnectedUser() {
-		if (Security.isConnected()) {
-			User user = User.find("byEmail", Security.connected()).first();
-			renderArgs.put("user", user);
-		}
-	}
+    @Before
+    static void setConnectedUser() {
+        if (Security.isConnected()) {
+            User user = User.find("byEmail", Security.connected()).first();
+            renderArgs.put("user", user);
+        }
+    }
+
+    public static void students() {
+        List<Student> students = Student.findAll();
+        List data = new ArrayList();
+
+        for (Student student : students) {
+            List innerList = new ArrayList(2);
+            innerList.add(student);
+            innerList.add(student.paidLessons - student.lessons.size());
+            data.add(innerList);
+        }
+
+        render(data);
+    }
+
 }
