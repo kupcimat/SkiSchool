439608cbdcfaa78b6bf1385f1e038e622e08399d
gias <sabol.miso@gmail.com>
2013-11-29 18:31:57 +0100

    FIX: Adding all relevant js and libraries

---
+++ jakaski/public/javascripts/locale_sk.js
@@ -0,0 +1,48 @@
+scheduler.locale = {
+	date: {
+		month_full: ["Január", "Február", "Marec", "Apríl", "Máj", "Jún", "Júl", "August", "September", "Október", "November", "December"],
+		month_short: ["Jan", "Feb", "Mar", "Apr", "Máj", "Jún", "Júl", "Aug", "Sep", "Okt", "Nov", "Dec"],
+		day_full: ["Neděle", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota"],
+		day_short: ["Ne", "Po", "Ut", "St", "Št", "Pi", "So"]
+	},
+	labels: {
+		dhx_cal_today_button: "Dnes",
+		day_tab: "Deň",
+		week_tab: "Týždeň",
+		month_tab: "Mesiac",
+		new_event: "",
+		icon_save: "Uložit",
+		icon_cancel: "Zpäť",
+		icon_details: "Detail",
+		icon_edit: "Edituj",
+		icon_delete: "Smazať",
+		confirm_closing: "", //Vaše změny budou ztraceny, opravdu ?
+		confirm_deleting: "Dostupnosť bude trvale zmazaná, pokračovať?",
+		section_description: "Poznámky",
+		section_time: "Doba platnosti",
+
+		/*recurring events*/
+		confirm_recurring: "Přejete si upravit celou řadu opakovaných událostí?",
+		section_recurring: "Opakování události",
+		button_recurring: "Vypnuto",
+		button_recurring_open: "Zapnuto",
+		button_edit_series: "Edit series",
+		button_edit_occurrence: "Upravit instance",
+
+		/*agenda view extension*/
+		agenda_tab: "Program",
+		date: "Dátum",
+		description: "Poznámka",
+
+		/*year view extension*/
+		year_tab: "Rok",
+		full_day: "Celý deň",
+
+		/*week agenda view extension*/
+		week_agenda_tab: "Program",
+
+		/*grid view extension*/
+		grid_tab: "Mriežka"
+	}
+};
+
